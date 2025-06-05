9074e661d634323bb8ff13d7d0c846a5daa0ed85
Mindp1 <manandp2@illinois.edu>
2025-06-05 14:08:24 -0500

    Initial Commit

---
+++ Electricity Price/Electricity_PriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Electricity_PriceApp.swift
+//  Electricity Price
+//
+//  Created by Manan Patel on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Electricity_PriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
